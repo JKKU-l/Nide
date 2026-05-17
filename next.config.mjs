80dc75e4bf04ae08c94ee5445d779016ecdff50c
infodevinto-stack <infodev.into@gmail.com>
2026-05-17 20:56:24 +0630

    update all changes

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
     unoptimized: true,
   },
   devIndicators: false,
+  allowedDevOrigins: ['http://10.38.128.80:3000', 'http://localhost:3000'],
 }
 
 export default nextConfig
